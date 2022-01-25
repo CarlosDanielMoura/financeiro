75df1ed29b5ac6557ac8c868cbcf7d37ca6cd5cf
Carlos_Daniel <58231139+CarlosDanielMoura@users.noreply.github.com>
2022-01-25 13:40:25 -0300

    Update parcelas.php

--- painel-adm/compras/parcelas.php
+++ painel-adm/compras/parcelas.php
@@ -11,33 +11,32 @@ $data = $_POST['data'];
 $parcelas = $_POST['parcelas'];
 
 if ($data == '') {
-    $data = date('Y-m-d');
+	$data = date('Y-m-d');
 }
 
 
 
-if($parcelas > 1){
+if ($parcelas > 1) {
 	$novo_valor = $valor / $parcelas;
-	for($i=1; $i <= $parcelas; $i++){
+	for ($i = 1; $i <= $parcelas; $i++) {
 
 		$resto_conta = $valor - $novo_valor * $parcelas;
-	
-		if($i == $parcelas){
+
+		if ($i == $parcelas) {
 			$novo_valor = $novo_valor + $resto_conta;
 		}
-	
-	
-		$query = $pdo->prepare("INSERT INTO contas_pagar set descricao = :descricao,
-		 data_emissao = curDate(), vencimento = :data, frequencia = 'Uma Vez', 
-		  valor = :valor, usuario_lanc = '$id_usuario', status = 'Pendente', 
-		  id_compra = '-1', arquivo = 'sem-foto.jpg'");
 
+
+		$query = $pdo->prepare("INSERT INTO contas_pagar set descricao = :descricao,
+		data_emissao = curDate(), vencimento = :data, frequencia = 'Uma Vez', 
+		valor = :valor, usuario_lanc = '$id_usuario', status = 'Pendente', 
+		id_compra = '-1', arquivo = 'sem-foto.jpg'");
 		$query->bindValue(":valor", "$novo_valor");
-		$query->bindValue(":descricao", "Parcela ".$i);
+		$query->bindValue(":descricao", "Parcela " . $i);
 		$query->bindValue(":data", "$data");
 		$query->execute();
 
-		$data = date('Y/m/d', strtotime("+1 month",strtotime($data)));
+		$data = date('Y/m/d', strtotime("+1 month", strtotime($data)));
 	}
 }
 
