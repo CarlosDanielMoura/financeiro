c30452fcd496bddcc091bdb911139bdedcdde8c1
Carlos_Daniel <58231139+CarlosDanielMoura@users.noreply.github.com>
2022-02-15 17:44:57 -0300

    ajuste

--- js/main.js
+++ js/main.js
@@ -76,3 +76,26 @@
     
 
 })(jQuery);
+
+$(function(){
+    $("#form-total").steps({
+        headerTag: "h2",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        autoFocus: true,
+        transitionEffectSpeed: 500,
+        titleTemplate : '<div class="title">#title#</div>',
+        labels: {
+            previous : 'Back Step',
+            next : 'Next Step',
+            finish : 'Submit',
+            current : ''
+        },
+    });
+    $("#date").datepicker({
+        dateFormat: "MM - DD - yy",
+        showOn: "both",
+        buttonText : '<i class="zmdi zmdi-chevron-down"></i>',
+    });
+});
